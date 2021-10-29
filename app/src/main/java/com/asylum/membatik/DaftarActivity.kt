3fadbb94e0fb86676711ab261ccddd861e75a2c8
yogaiw <agoy.indra@gmail.com>
2021-10-29 23:04:31 +0700

    ui daftar

--- app/src/main/java/com/asylum/membatik/DaftarActivity.kt
+++ app/src/main/java/com/asylum/membatik/DaftarActivity.kt
@@ -4,15 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_daftar.*
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class DaftarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_daftar)
 
-        tv_daftar.setOnClickListener {
-            val i = Intent(this, DaftarActivity::class.java)
+        tv_masuk.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
             startActivity(i)
         }
     }
