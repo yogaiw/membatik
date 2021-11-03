74324e60eb7f2dd121c46c07cd2d9263e20dce3a
yogaiw <agoy.indra@gmail.com>
2021-11-03 23:47:07 +0700

    detail produk ui done

--- app/src/main/java/com/asylum/membatik/adapter/ProdukAdapter.kt
+++ app/src/main/java/com/asylum/membatik/adapter/ProdukAdapter.kt
@@ -1,12 +1,17 @@
 package com.asylum.membatik.adapter
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.asylum.membatik.DetailProduk
 import com.asylum.membatik.R
 import com.asylum.membatik.model.ProdukModel
 import kotlinx.android.synthetic.main.item_produk.view.*
@@ -21,19 +26,19 @@ class ProdukAdapter(private val listMyData: ArrayList<ProdukModel>, val context:
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         holder.bind(listMyData[position])
 
-//        holder.cv.setOnClickListener {
-//            val intent = Intent(context, DetailActivity::class.java)
-//            intent.putExtra("POSISI", position.toString())
-//            context.startActivity(intent)
-//
-//            Log.d(TAG, position.toString())
-//        }
+        holder.item.setOnClickListener {
+            val intent = Intent(context, DetailProduk::class.java)
+            intent.putExtra("PRODUK", listMyData[position].judul)
+            intent.putExtra("HARGA", listMyData[position].harga)
+            context.startActivity(intent)
+            Log.d(TAG, listMyData[position].judul)
+        }
     }
 
     override fun getItemCount(): Int = listMyData.size
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val cv: CardView = itemView.findViewById(R.id.cv_item)
+        val item: ConstraintLayout = itemView.findViewById(R.id.produk_item)
 
         fun bind(myData: ProdukModel) {
             with(itemView){
